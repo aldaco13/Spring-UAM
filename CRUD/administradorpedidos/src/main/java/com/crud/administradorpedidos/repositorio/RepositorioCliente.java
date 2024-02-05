7518c2212313a0a4854c3691dd7357e73e70aef3
aldaco13 <omar.aldaco.m@gmail.com>
2024-02-04 21:50:11 -0600

    feat(ServicioCliente)Se agrega generador de clientes

--- CRUD/administradorpedidos/src/main/java/com/crud/administradorpedidos/repositorio/RepositorioCliente.java
+++ CRUD/administradorpedidos/src/main/java/com/crud/administradorpedidos/repositorio/RepositorioCliente.java
@@ -3,6 +3,7 @@ package com.crud.administradorpedidos.repositorio;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.crud.administradorpedidos.entidades.Cliente;
 
@@ -13,5 +14,8 @@ public interface RepositorioCliente extends JpaRepository<Cliente, Integer> {
 	public Optional<Cliente> findByCliente(Long cliente);
 	
 	public Optional<Cliente> findByCorreo(String correo);
+	
+	@Query(value = "SELECT MAX(id) FROM Cliente", nativeQuery = true)
+    Long encontrarUltimoId();
 	
 }
